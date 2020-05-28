94ba51c8fb686223ff2accd6927bb90863edb513
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-05-28 17:35:55 -0600

    added server.js file and set up server bioler plate

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`We are listening on Port: ${PORT} My DOOD!` )
+});
